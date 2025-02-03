481d75b976acff9e96f49823be32e3d9e876cdf1
IndraNaik <pathlavath.indra@gmail.com>
2025-02-03 10:00:55 +0530

    backup scripts

---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+<< help
+This is a shell script to take backups 
+This can also be used with cron
+help
+
+source_dir="/home/ubuntu/scripts"
+destination_dir="/home/ubuntu/backups"
+
+function create_backup {
+	timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
+
+	backup_dir="${destination_dir}/backup_${timestamp}"
+
+	zip -r "${backup_dir}.zip" "$source_dir"
+
+	echo "Backup completed"
+}
+
+create_backup $source_dir $destination_dir
